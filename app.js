884628635c17cb3b9c3f7b3a843a60b6bf0992d3
Ignat <ignat.mustafin@taqtile.com>
2021-11-28 21:56:53 +0300

    test4

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('PREMSHOP100000')
+    res.send('hello sasha')
 })
 
 app.listen(PORT, () => {
