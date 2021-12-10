ebd2cff4d4ae8c717eff1cd6d069be22c471a5e3
Ignat <ignat.mustafin@taqtile.com>
2021-12-10 22:37:18 +0300

    router config

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import router from './routes/auth.router.js';
+import authRouter from './routes/auth.router.js';
 import path from 'path'
 import cors from 'cors'
 import config from '../config/config.js'
@@ -12,18 +12,15 @@ const app = express()
 app.use(cors())
 app.options('*', cors())
 app.use(express.json())
-app.use('/auth', router)
+app.use(express.static(config.clientPath))
+app.use(authRouter)
 
 
-
+ 
 app.get('/', (req, res) => {
     res.sendFile(`${config.clientPath}main.html`)
 })
 
-app.get('/auth/signIn', (req, res) => {
-    res.sendFile(`${config.clientPath}auth.html`)
-})
-
 app.listen(PORT, () => {
-    console.log(`Server started on port ${config.clientPath}`)
+    console.log(`Server started on port ${(path.join(config.clientPath + "views"))}`)
 })
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -58,6 +58,7 @@ class authController {
         config.jwt,
         { expiresIn: 60 * 60 }
       );
+      res.json({success: true, token: token})
       
     } catch (error) {
       console.log(error)
