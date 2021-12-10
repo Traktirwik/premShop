4d2e7729305c2ebe7265f150ef317a2b7b8d0a96
AMikhnyuk <amikhnyuk@xbsoftware.by>
2021-12-11 00:00:44 +0300

    little changes

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import authRouter from './routes/auth.router.js';
 import path from 'path'
 import cors from 'cors'
 import config from '../config/config.js'
+import itemRouter from './routes/item.router.js';
 
 
 console.log(path)
@@ -14,9 +15,10 @@ app.options('*', cors())
 app.use(express.json())
 app.use(express.static(config.clientPath))
 app.use(authRouter)
+app.use(itemRouter)
 
 
- 
+
 app.get('/', (req, res) => {
     res.sendFile(`${config.clientPath}main.html`)
 })
