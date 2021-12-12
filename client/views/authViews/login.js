88d49b88dcd2831e425af9b31c1c88d41c8a056b
Ignat <ignat.mustafin@taqtile.com>
2021-12-13 00:20:57 +0300

    added styles for auth pages. transition between pages #signIn and #registration, added admin functions for put and delete items

--- client/views/authViews/login.js
+++ client/views/authViews/login.js
@@ -2,19 +2,23 @@ import {root} from '../../auth.js'
 import {getToken} from "../request/axios.js"
 
 const login = () => {
+    sessionStorage.setItem("is_reloaded", false);
+
     root.innerHTML = `
-    <div>
-        <span>Sign In</span>
-        <span>Email</span>
-        <input id="email" type="text">
-        <span>Password</span>
-        <input id="password" type="text">
-        <button id="butt" type="button">sign In</button>
+    <div class="auth_wrapper">
+        <span id="sign_in" class="auth_title">ВХОД В PREMIUM SHOP</span>
+        <input id="email" class="auth_input" type="text" placeholder="E-mail или имя">
+        <input id="password" class="auth_input" type="text" placeholder="Пароль">
+        <div class="checkBox">
+            <input type="checkbox" class="termsBox">
+            <label for="termsBox"">Я принимаю Пользовательское соглашение</label>
+        </div>
+        <button id="butt" type="button">ВОЙТИ</button>
+        <span id="registration" class="link_field">Нет аккаунта? Зарегистрируйтесь</span>
     </div>
     `
 
     const butt = document.getElementById("butt")
-
 
     butt.onclick = async function() {
         const email = document.getElementById("email")
@@ -27,10 +31,15 @@ const login = () => {
         
         if(response.data.success == true) {
             localStorage.setItem("token", JSON.stringify(response.data.token))
+            localStorage.setItem("role", JSON.stringify(response.data.role.role))
         } 
         if(localStorage.token) {
             window.location.href = "/"
         }     
+    }
+    const registrationButt = document.getElementById("registration")
+    registrationButt.onclick = () => {
+        window.location.href = "#registration"
     }
 }
 
--- server/routes/item.router.js
+++ server/routes/item.router.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import CommonController from "../controllers/common.controller.js";
 import ItemsController from "../controllers/items.controller.js";
 import Items from "../models/items.model.js";
+import roleMdw from "../middlewares/role.mdw.js";
 
 
 const itemRouter = new Router()
@@ -11,7 +12,7 @@ const itemController = new CommonController(Items)
 itemRouter.get("/items", (req, res) => itemController.getAll(req, res))
 itemRouter.get("/items/:id", (req, res) => ItemsController.getHtml(req, res))
 itemRouter.post("/items", (req, res) => itemController.create(req, res))
-itemRouter.put("/items/:id", (req, res) => itemController.updateById(req, res))
-itemRouter.get("/items/:id", (req, res) => itemController.deleteById(req, res))
+itemRouter.put("/items/:id", roleMdw(["ADMIN"]), (req, res) => itemController.updateById(req, res))
+itemRouter.delete("/items/:id", roleMdw(["ADMIN"]), (req, res) => itemController.deleteById(req, res))
 
 export default itemRouter
